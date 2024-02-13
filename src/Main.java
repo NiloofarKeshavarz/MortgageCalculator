b11950be705011e5a99a7d8ac29779b98c4869aa
NiloofarKeshavarz <niloofar.keshavarz@gmail.com>
2024-02-13 13:16:36 -0500

    Creates the simple calculator

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,34 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println(
-                "HELLO WORLD!!"
-        );
-        System.out.println("SECOND commit WORKS!!");
+        final byte Months_In_YEAR = 12;
+        final byte PERCENT = 100;
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Principal:");
+        int principal =input.nextInt();
+
+        System.out.println("Annual Intrest Rate: ");
+        float annualInterest = input.nextFloat();
+        float monthlyInterest = annualInterest/PERCENT /Months_In_YEAR;
+
+        System.out.println("Period(years): ");
+        byte years = input.nextByte();
+        int numberOfPayments = years *Months_In_YEAR;
+
+
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
+
+
     }
 }

3299218d31ae456d95dcb7b8161ab2c79cc0301d
NiloofarKeshavarz <niloofar.keshavarz@gmail.com>
2024-02-14 14:26:17 -0500

    Overloads the readNumber method

--- src/Console.java
+++ src/Console.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
 
 public class Console {
+    private static Scanner input = new Scanner(System.in);
+    public  static double readNumber(String prompt){
+        return  input.nextDouble();
+    }
     public static double readNumber(String prompt, double min, double max) {
-        Scanner input = new Scanner(System.in);
         double value;
         while (true) {
             System.out.println(prompt);
-            value = input.nextFloat();
+            value = input.nextDouble();
             if (value > min && value <= max)
                 break;
             System.out.println("Enter a value  between" + min + " and" + max);

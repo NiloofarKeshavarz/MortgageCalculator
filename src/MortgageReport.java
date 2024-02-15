9e08cf23a9c151425b6f225fa88932dcc0ebbf34
NiloofarKeshavarz <niloofar.keshavarz@gmail.com>
2024-02-15 10:01:28 -0500

    Refactors NumberFormat field in the constructor

--- src/MortgageReport.java
+++ src/MortgageReport.java
@@ -2,16 +2,18 @@ import java.text.NumberFormat;
 
 public class MortgageReport {
 
+    private final NumberFormat currency;
     private  MortgageCalculator calculator;
 
     public MortgageReport(MortgageCalculator calculator) {
         this.calculator = calculator;
+        currency = NumberFormat.getCurrencyInstance();
     }
 
     public  void printMortgagePayment() {
         double mortgage = calculator.calculateMortgage();
 
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        String mortgageFormatted = currency.format(mortgage);
         System.out.println();
         System.out.println("MORTGAGE");
         System.out.println("-----------");
@@ -23,7 +25,7 @@ public class MortgageReport {
         System.out.println("PAYMENT SCHEDULE");
         System.out.println("-----------");
         for(double balance: calculator.getRemainingBalances())
-            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+            System.out.println(currency.format(balance));
 //        for (short month = 1; month <= calculator.getYears() * MortgageCalculator.Months_In_YEAR; month++) {
 //            double balance = calculator.calculateBalance(month);
 //            String balanceFormatted = NumberFormat.getCurrencyInstance().format(balance);
